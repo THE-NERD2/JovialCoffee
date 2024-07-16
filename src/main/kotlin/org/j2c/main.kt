f9627f5446e5e864fbab33f8cb75b32840e430b8
payton <pdp9729@gmail.com>
2024-07-16 12:08:36 -0600

    Print method instructions

--- src/main/kotlin/org/j2c/main.kt
+++ src/main/kotlin/org/j2c/main.kt
@@ -1,22 +1,44 @@
 package org.j2c
 
+import javassist.ClassPool
+import javassist.CtClass
+import javassist.NotFoundException
+import javassist.bytecode.Mnemonic
 import org.j2c.ast.NClass
 import java.io.File
 import java.net.URLClassLoader
+import kotlin.reflect.KClass
 import kotlin.reflect.KFunction
 import kotlin.reflect.KProperty
 import kotlin.reflect.jvm.javaField
 
 fun parse(path: String, name: String): NClass {
-    val classLoader = URLClassLoader(arrayOf(File(path).toURI().toURL()), ClassLoader.getSystemClassLoader())
-    val clazz = classLoader.loadClass(name).kotlin
+    val clazz: KClass<*>
+    val nclass: NClass
+    val ctclass: CtClass
 
-    val nclass = NClass(clazz.simpleName!!)
+    val classLoader = URLClassLoader(arrayOf(File(path).toURI().toURL()), ClassLoader.getSystemClassLoader())
+    clazz = classLoader.loadClass(name).kotlin
+    nclass = NClass(clazz.simpleName!!)
+    val pool = ClassPool()
+    pool.appendClassPath(path)
+    ctclass = pool.get(name)
+
     clazz.members.forEach {
         if(it is KProperty) {
             nclass.NFieldDeclaration(it.name, (it.javaField?.type ?: it.returnType).toString())
         } else if(it is KFunction) {
-            nclass.NMethodDeclaration(it.name, it.returnType.toString(), it.parameters.map { it.type.toString() })
+            try {
+                // Process method code
+                val instructions = ctclass.getDeclaredMethod(it.name).methodInfo.codeAttribute.iterator()
+                while (instructions.hasNext()) {
+                    val pos = instructions.next()
+                    val opcode = instructions.byteAt(pos)
+                    println(Mnemonic.OPCODE[opcode])
+                    // TODO: create AST from opcodes
+                }
+                nclass.NMethodDeclaration(it.name, it.returnType.toString(), it.parameters.map { it.type.toString() })
+            } catch(_: NotFoundException) {}
         }
     }
     return nclass
