1c7716c07261eaccc029150249c39e0de74608cc
payton <pdp9729@gmail.com>
2024-07-16 10:48:13 -0600

    Updated DummyClass to better suit near-future bytecode analysis

--- src/test/resources/org/j2c/DummyClass.java
+++ src/test/resources/org/j2c/DummyClass.java
@@ -2,7 +2,8 @@ package org.j2c;
 
 public class DummyClass {
     public int randomField = 0;
-    public void printField() {
-        System.out.println(randomField);
+    public int outputField;
+    public void sendField() {
+        outputField = randomField;
     }
 }
