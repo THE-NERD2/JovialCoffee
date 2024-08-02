5063598527b2fd86bea8e69bbe7e921c3fde16b0
payton <pdp9729@gmail.com>
2024-08-01 18:59:59 -0600

    Follow ifs and add else statements

--- src/main/kotlin/org/j2c/assembly/rules/if.kt
+++ src/main/kotlin/org/j2c/assembly/rules/if.kt
@@ -7,17 +7,20 @@ object IF {
     val IFEQ = Rule(Opcode.IFEQ) { instructions, pos, _, _, stack ->
         val v = stack.pop()
         val i = instructions.s16bitAt(pos + 1)
-        stack.add("if($v == 0) goto ${pos + i}")
+        stack.add("if($v == 0)")
+        GOTO.follow(instructions, pos, i, true)
     }
     val IFNE = Rule(Opcode.IFNE) { instructions, pos, _, _, stack ->
         val v = stack.pop()
         val i = instructions.s16bitAt(pos + 1)
-        stack.add("if($v != 0) goto ${pos + i}")
+        stack.add("if($v != 0)")
+        GOTO.follow(instructions, pos, i, true)
     }
     val IFGE = Rule(Opcode.IFGE) { instructions, pos, _, _, stack ->
         val i = instructions.s16bitAt(pos + 1)
         val v = stack.pop()
-        stack.add("if($v >= 0) goto ${pos + i}")
+        stack.add("if($v >= 0)")
+        GOTO.follow(instructions, pos, i, true)
     }
 }
 
@@ -26,18 +29,21 @@ object IF_A {
     val IFNONNULL = Rule(Opcode.IFNONNULL) { instructions, pos, _, _, stack ->
         val v = stack.pop()
         val i = instructions.s16bitAt(pos + 1)
-        stack.add("if($v != null) goto ${pos + i}")
+        stack.add("if($v != null)")
+        GOTO.follow(instructions, pos, i, true)
     }
     val IF_ACMPEQ = Rule(Opcode.IF_ACMPEQ) { instructions, pos, _, _, stack ->
         val i = instructions.s16bitAt(pos + 1)
         val v2 = stack.pop()
         val v1 = stack.pop()
-        stack.add("if($v1 == $v2) goto ${pos + i}")
+        stack.add("if($v1 == $v2)")
+        GOTO.follow(instructions, pos, i, true)
     }
     val IF_ACMPNE = Rule(Opcode.IF_ACMPNE) { instructions, pos, _, _, stack ->
         val i = instructions.s16bitAt(pos + 1)
         val v2 = stack.pop()
         val v1 = stack.pop()
-        stack.add("if($v1 != $v2) goto ${pos + i}")
+        stack.add("if($v1 != $v2)")
+        GOTO.follow(instructions, pos, i, true)
     }
 }
