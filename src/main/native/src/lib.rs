2717faedf9620fc3ae1b086760b0c810b391e7ed
payton <pdp9729@gmail.com>
2024-10-12 14:20:33 -0600

    Native methods work!
    Comes at a cost. No more C++; now pure Rust, which is not great since the JVM backend is written in C/C++. There was no way to export C/C++ symbols with CXX crate, though, so here we are.

--- src/main/native/src/lib.rs
+++ src/main/native/src/lib.rs
@@ -1,50 +1,15 @@
-use crate::ffi::SomePrimitive;
+use jni::JNIEnv;
+use jni::objects::JObject;
 
-#[cxx::bridge]
-mod ffi {
-    struct SomePrimitive {
-        value_type: UniquePtr<CxxString>,
-        bool_value: bool,
-        byte_value: i8,
-        short_value: i16,
-        int_value: i32,
-        long_value: i64,
-        float_value: f32,
-        double_value: f64,
-        string_value: UniquePtr<CxxString>
-    }
-    extern "Rust" {
-        fn start_querying();
-        fn generate_code();
-        fn finalize_ast();
-    }
-    unsafe extern "C++" {
-        include!("native/include/j2c.h");
-
-        fn getNodeType() -> UniquePtr<CxxString>;
-        fn dropInto(signature: String, field: String);
-        fn stepOut();
-        fn getPrimitive(signature: String, field: String) -> SomePrimitive;
-    }
+#[no_mangle]
+pub extern "system" fn Java_org_j2c_llvm_LLVM_createAST<'a>(mut env: JNIEnv<'a>, obj: JObject<'a>, root: JObject<'a>) {
+    println!("Creating AST");
 }
-fn get_node_type() -> String {
-    return ffi::getNodeType().to_string();
+#[no_mangle]
+pub extern "system" fn Java_org_j2c_llvm_LLVM_generateCurrentAST() {
+    println!("Generating code for current AST");
 }
-fn drop_into(signature: &str, field: &str) {
-    ffi::dropInto(String::from(signature), String::from(field));
-}
-fn step_out() {
-    ffi::stepOut();
-}
-fn get_primitive(signature: &str, field: &str) -> SomePrimitive {
-    return ffi::getPrimitive(String::from(signature), String::from(field));
-}
-pub fn start_querying() {
-    println!("Started querying");
-}
-pub fn generate_code() {
-    println!("Generating code");
-}
-pub fn finalize_ast() {
-    println!("Finalizing AST");
+#[no_mangle]
+pub extern "system" fn Java_org_j2c_llvm_LLVM_finishCodeGen() {
+    println!("Finishing code generation");
 }
--- src/test/kotlin/org/j2c/test.kt
+++ src/test/kotlin/org/j2c/test.kt
@@ -19,11 +19,22 @@ class Tests {
         printAll()
     }
     @Test
+    fun DummyClass_main() {
+        main(arrayOf("/home/payton/Coding/apis/libs/J2C/src/test/resources", "org.j2c.DummyClass"))
+        if(getClasses().size == 0) {
+            main(arrayOf("/home/payton/IdeaProjects/J2C/src/test/resources", "org.j2c.DummyClass"))
+        }
+    }
+    @Test
     fun EncryptedStorage() { // from Notebook repository
         init("/home/payton/IdeaProjects/Notebook/build/libs/Notebook-1.0-SNAPSHOT-all.jar")
         parse("org.notebook.EncryptedStorage")!!
         getClasses().forEach { println(it.toString()) }
         printAll()
+    }
+    @Test
+    fun EncryptedStorage_main() {
+        main(arrayOf("/home/payton/IdeaProjects/Notebook/build/libs/Notebook-1.0-SNAPSHOT-all.jar", "org.notebook.EncryptedStorage"))
     }
     @Test
     fun WorkingFile() {
