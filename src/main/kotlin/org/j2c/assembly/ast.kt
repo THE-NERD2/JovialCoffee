081c05f3a619a69ff50c14f897af9599ae864f5d
payton <pdp9729@gmail.com>
2024-10-20 14:04:09 -0600

    Added shallow-level parsing of NFieldDeclaration and NMethodDeclaration

--- src/main/kotlin/org/j2c/assembly/ast.kt
+++ src/main/kotlin/org/j2c/assembly/ast.kt
@@ -52,12 +52,17 @@ class NMethodDeclaration(
     val clazz: NClass,
     val name: String,
     val ret: String,
-    val args: Collection<String>,
-    val body: Collection<Node>
+    val args: ArrayList<String>,
+    val body: ArrayList<Node>
 ): Node("NMethodDeclaration") {
     init {
         clazz.methods.add(this)
     }
+    // Next four for use in JNI
+    fun numArgs() = args.size
+    fun getArg(index: Int) = args[index]
+    fun bodySize() = body.size
+    fun getBodyElement(index: Int) = body[index]
     override fun toString(): String {
         var str = ""
         body.forEach {
