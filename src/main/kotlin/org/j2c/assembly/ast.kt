ec3a8701d2f087e9578c9d062e499ac36d4b156b
payton <pdp9729@gmail.com>
2024-10-26 16:37:31 -0600

    Fixed NClass id counting

--- src/main/kotlin/org/j2c/assembly/ast.kt
+++ src/main/kotlin/org/j2c/assembly/ast.kt
@@ -15,9 +15,10 @@ class NNull: Node("NNull") {
 
 class NClass(val qualName: String, val name: String, addToClasses: Boolean = true): Node("NClass") {
     companion object {
-        internal var lastId = -1
+        internal var lastId = 0
+        internal val idDictionary = mutableMapOf<String, Int>()
     }
-    val id = ++lastId
+    val id: Int
     val fields = arrayListOf<NFieldDeclaration>()
     val methods = arrayListOf<NMethodDeclaration>()
     // These four are to be called from JNI
@@ -26,7 +27,10 @@ class NClass(val qualName: String, val name: String, addToClasses: Boolean = tru
     fun numMethods() = methods.size
     fun getMethod(index: Int) = methods[index]
     init {
+        id = idDictionary[qualName] ?: lastId++
         if(addToClasses) classes.add(this)
+        // Register name for future NClasses
+        idDictionary[qualName] = id
     }
     val cname get() = "$name$id"
     override fun toString(): String {
