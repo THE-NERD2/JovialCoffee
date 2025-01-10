99578e0d502b8858a08b07a3f533873dec4db1a8
payton <pdp9729@gmail.com>
2025-01-10 16:35:35 -0700

    Deleted main method based tests in favor of regular run configurations

--- src/test/kotlin/org/j2c/test.kt
+++ src/test/kotlin/org/j2c/test.kt
@@ -16,31 +16,15 @@ class Tests {
         printAll()
     }
     @Test
-    fun DummyClass_main() {
-        try {
-            main(arrayOf("/home/payton/Coding/apis/libs/J2C/src/test/resources", "org.j2c.DummyClass"))
-        } catch(_: NullPointerException) {
-            main(arrayOf("/home/payton/IdeaProjects/J2C/src/test/resources", "org.j2c.DummyClass"))
-        }
-    }
-    @Test
     fun EncryptedStorage() { // from Notebook repository
         init("/home/payton/IdeaProjects/Notebook/build/libs/Notebook-1.0-SNAPSHOT-all.jar")
         parseAndRunForEachClass("org.notebook.EncryptedStorage") { println(it.toString()) }
         printAll()
     }
     @Test
-    fun EncryptedStorage_main() {
-        main(arrayOf("/home/payton/IdeaProjects/Notebook/build/libs/Notebook-1.0-SNAPSHOT-all.jar", "org.notebook.EncryptedStorage"))
-    }
-    @Test
     fun WorkingFile() {
         init("/home/payton/IdeaProjects/Notebook/build/libs/Notebook-1.0-SNAPSHOT-all.jar")
         parseAndRunForEachClass("org.notebook.WorkingFile") { println(it.toString()) }
         printAll()
-    }
-    @Test
-    fun WorkingFile_main() {
-        main(arrayOf("/home/payton/IdeaProjects/Notebook/build/libs/Notebook-1.0-SNAPSHOT-all.jar", "org.notebook.WorkingFile"))
     }
 }
