1c06f541e21f9b045beace0cedfa6aae03f96924
payton <pdp9729@gmail.com>
2025-05-28 15:25:37 -0600

    Fixed more build craziness

--- native/src/lib.cpp
+++ native/src/lib.cpp
@@ -1,11 +1,19 @@
 #include <jni.h>
+#include <vector>
 #include <iostream>
 
-extern "C" void Java_org_j2c_llvm_LLVM_addClassAST(JNIEnv* env, jclass clazz, jobject root) {
-    std::cout << "1\n";
-    return;
-}
-extern "C" void Java_org_j2c_llvm_LLVM_compile() {
-    std::cout << "2\n";
-    return;
+using namespace std;
+
+static vector<jobject> objects;
+
+extern "C" {
+    JNIEXPORT void JNICALL Java_org_j2c_llvm_LLVM_addClassAST(JNIEnv* env, jclass clazz, jobject obj) {
+        cout << "1\n";
+        objects.push_back(obj);
+        return;
+    }
+    JNIEXPORT void JNICALL Java_org_j2c_llvm_LLVM_compile() {
+        cout << "2\n";
+        return;
+    }
 }
